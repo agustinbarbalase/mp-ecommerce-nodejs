383d3f076cb5a182c25757976b5d5da4d65e6d34
AgusBarba03 <agustinbarbalase@gmail.com>
2022-02-17 18:21:21 -0300

    modify handlebars config and express listen

--- app.js
+++ app.js
@@ -1,22 +1,29 @@
-var express = require('express');
-var exphbs  = require('express-handlebars');
-var port = process.env.PORT || 3000
+const express = require("express");
+const exphbs = require("express-handlebars");
+const PORT = process.env.PORT || 3000;
 
-var app = express();
- 
-app.engine('handlebars', exphbs());
-app.set('view engine', 'handlebars');
+const app = express();
 
-app.use(express.static('assets'));
- 
-app.use('/assets', express.static(__dirname + '/assets'));
+app.engine(
+  "handlebars",
+  exphbs({
+    extname: ".hbs",
+  })
+);
+app.set("view engine", "handlebars");
 
-app.get('/', function (req, res) {
-    res.render('home');
+app.use(express.static("assets"));
+
+app.use("/assets", express.static(__dirname + "/assets"));
+
+app.get("/", function (req, res) {
+  res.render("home");
 });
 
-app.get('/detail', function (req, res) {
-    res.render('detail', req.query);
+app.get("/detail", function (req, res) {
+  res.render("detail", req.query);
 });
 
-app.listen(port);
+app.listen(port, () => {
+  console.log(`Server on port ${PORT}`);
+});
